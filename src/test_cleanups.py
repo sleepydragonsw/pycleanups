cc9520a9d2767a62fc15de92747fc0237f4bc110
Denver Coneybeare <denver.coneybeare@gmail.com>
2010-08-13 03:17:51 -0400

    implemented unit tests for CleanupListener

--- src/test_cleanups.py
+++ src/test_cleanups.py
@@ -1,2 +1,27 @@
-from cleanups import *
+import cleanups
 import unittest
+
+################################################################################
+
+class CleanupsTestCase(unittest.TestCase):
+    """
+    Base class for all test cases for the "cleanups" library.
+    """
+    pass
+
+################################################################################
+
+class TestCleanupListener(CleanupsTestCase):
+    """
+    Tests the CleanupListener class.  Since this class is just 3 empty methods,
+    try and invoke those methods and ensure that they return None.
+    """
+
+    def test(self):
+        x = cleanups.CleanupListener()
+        self.assertIsNone(x.starting(None, None))
+        self.assertIsNone(x.completed(None, None, None))
+        self.assertIsNone(x.failed(None, None, None))
+
+if __name__ == "__main__":
+    unittest.main()
